faef3527e800e88a9ed4a8fd6969ae23002c9537
Kenneth Chew <79120643+kthchew@users.noreply.github.com>
2023-11-12 23:38:54 -0500

    Initial commit

---
+++ CacheSimulation/CacheSimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CacheSimulationApp.swift
+//  CacheSimulation
+//
+//  Created by Kenneth Chew on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct CacheSimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
